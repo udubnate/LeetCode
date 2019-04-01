815aac9815c4720c2bab1b01e011562922fbda12
Nate Bruneau <nathbr@microsoft.com>
2019-04-01 16:25:42 -0700

    first checkin after dotnet add

--- csharp/344-Reverse-String.cs
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public void ReverseString(char[] s) {
-        n = new char[s.Length];
-        for (int i = 0; i < s.Length-1; i++){
-            n += s[s.Length-1-i];
-        }
-        return n;
-    }
-}
