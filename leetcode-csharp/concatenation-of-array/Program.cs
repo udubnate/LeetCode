3af96189f7164aad7103c407adfa2fb7102b87b1
udubnate <natebruneau@gmail.com>
2022-03-05 10:24:55 -0800

    Completed concatenation of array problem this morning

---
+++ leetcode-csharp/concatenation-of-array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace concatenation_of_array
+{
+    internal class Program
+    {
+        //Concatenation of Array
+        /*
+         *  Input: nums = [1,2,1]
+         *  Output: [1,2,1,1,2,1]
+         */
+
+        static int[] ConcatenationofArray(int[] nums)
+        {
+
+            int[] outarr = new int[2 * nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                outarr[i] = nums[i];
+                outarr[nums.Length + i] = nums[i];
+            }
+            return outarr;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 1 };
+
+            Console.WriteLine(String.Join(',', ConcatenationofArray(nums)));
+        }
+    }
+}
---
+++ leetcode-csharp/leetcode-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcode_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
