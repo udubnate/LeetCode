bf946b3f565b3fb9e92f2df3ee9c9cfd3506327d
udubnate <natebruneau@gmail.com>
2022-03-08 11:20:47 -0800

    triangle

---
+++ leetcode-csharp/largest-perimeter-triangle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace largest_perimeter_triangle
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(2, 1, 2);
+            Test(1, 2, 1);
+            Test(3, 2, 3, 4);
+        }
+
+        private static void Test(params int[] n)
+        {
+            Solution s = new Solution();
+            var xout = s.LargestPerimeter(n);
+            Console.WriteLine("Input " + String.Join(',', n) + " : Answer is " + xout);
+        }
+    }
+
+    public class Solution
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            // in order to be a triangle sum of two greater than largest side
+            Array.Sort(nums);
+            
+            for (int i = nums.Length -1; i > 1; i--)
+            {
+                if (nums[i-2] + nums[i-1] > nums[i])
+                {
+                    return nums[i] + nums[i - 1] + nums[i - 2];
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            return 0;
+        }
+    }
+}
--- leetcode-csharp/subtract-the-product-and-sum-of-digits-of-an-integer/Program.cs
+++ leetcode-csharp/subtract-the-product-and-sum-of-digits-of-an-integer/Program.cs
@@ -6,7 +6,34 @@ namespace subtract_the_product_and_sum_of_digits_of_an_integer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Test(234);
+            Test(4421);
+        }
+
+        private static void Test(int n)
+        {
+            Solution s = new Solution();
+            var xout = s.SubtractProductAndSum(n);
+            Console.WriteLine("Input " + String.Join(',', n) + " : Answer is " + xout);
+        }
+    }
+
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int mult = 1;
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int num = n % 10;
+                mult *= num;
+                sum += num;
+                n /= 10;
+            }
+            return mult - sum;
         }
     }
 }
+
