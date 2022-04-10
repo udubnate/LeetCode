ed91ee476dd09a94ee9ceb31dca687f9999ab9bb
udubnate <natebruneau@gmail.com>
2022-04-10 09:18:25 -0700

    added some more solutions and unit tests

--- csharp/csharp-leetcode/csharp-leetcode.Tests/Tests.cs
+++ csharp/csharp-leetcode/csharp-leetcode.Tests/Tests.cs
@@ -32,5 +32,29 @@ namespace csharp_leetcode.Tests
             var test2 = remdup.RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 });
             Assert.That(test2, Is.EqualTo(5));
         }
+
+        [Test]
+        public void climbing_stairs_test()
+        {
+            climbing_stairs obj = new climbing_stairs();
+
+            var test1 = obj.ClimbStairs(5);
+            Assert.That(test1, Is.EqualTo(8));
+
+            var test2 = obj.ClimbStairs(3);
+            Assert.That(test2, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void baseball_game_test()
+        {
+            baseball_game obj = new baseball_game();
+
+            var test1 = obj.CalPoints(new string[] { "5", "2", "C", "D", "+" });
+            Assert.That(test1, Is.EqualTo(30));
+
+            var test2 = obj.CalPoints(new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" });
+            Assert.That(test2, Is.EqualTo(27));
+        }
     }
 }
---
+++ csharp/csharp-leetcode/csharp-leetcode/baseball_game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_leetcode
+{
+
+    // https://leetcode.com/problems/baseball-game/
+    // Note I used List, in reviewing the solutions Stack would have been probably a better data structure
+    // due to the fact you are accessing last element a bunch
+    public class baseball_game
+    {
+        public int CalPoints(string[] ops)
+        {
+            //generate output list
+            List<int> points = new List<int>();
+            //parse string
+            for (int i = 0; i < ops.Length; i++)
+            {
+                switch (ops[i])
+                {
+                    case "D":
+                        points.Add(points[points.Count - 1] * 2); 
+                        break;
+                    case "C": 
+                        points.RemoveAt(points.Count-1); 
+                        break;
+                    case "+": 
+                        var newScore = points[points.Count - 1] + points[points.Count - 2];
+                        points.Add(newScore);
+                        break;
+                    default:
+                        points.Add(Int32.Parse(ops[i])); 
+                        break;
+                }
+            }
+
+            return points.Sum();
+        }
+    }
+}
