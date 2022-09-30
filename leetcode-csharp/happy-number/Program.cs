4063374a8f277b95268b9f3f725b2e35b42881a6
udubnate <natebruneau@gmail.com>
2022-09-29 17:24:20 -0700

    commit local

---
+++ leetcode-csharp/happy-number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace happy_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(19);
+            Test(2);
+        }
+
+        private static void Test(params int[] n)
+        {
+            Solution s = new Solution();
+            var xout = s.IsHappy(n);
+            Console.WriteLine("Input " + String.Join(',', n) + " : Answer is " + xout);
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsHappy(int n)
+        {
+            return false;
+        }
+    }
+}
